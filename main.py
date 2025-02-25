2f8a2fa1b06b974ce491ef02299e9ff58c2f24cb
Virendra Kumar <68949418+baazviru@users.noreply.github.com>
2025-02-25 15:56:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def extract_url(video_url: str = Query(..., description="YouTube video URL")):
         ydl_opts = {
             'format': 'best',
             'cookiefile': 'cookies.txt'  # Pass the cookies for authentication
+            'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'Accept-Language': 'en-US,en;q=0.9'
+        }
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
