b5b7cff42d5b98e35e7866fe5c7f368dda4b9e38
Virendra Kumar <68949418+baazviru@users.noreply.github.com>
2025-03-14 11:47:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from pydantic import BaseModel
 import os
 import subprocess
 
+from flask import Flask, request
+
+
+
 from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 import yt_dlp
@@ -13,10 +17,34 @@ print(f"Cookies Path: {cookies_path}")  # Yeh line add karo
 cookies = "SID=g.a000uAh8uWhBiwVmfPnvLoGKed8m1PuIUg-ITw25SuITK9g0vIH1oqbRbWv2NrKIGjynQCG0ZgACgYKAZYSARcSFQHGX2MikQHnF9HLGEsMjvHjKrNidRoVAUF8yKpf4xJps_6MjjM0IZNEPDgS0076; HSID=A0QSgbsEb1Rba-_N3; SSID=A-pgr3Witnyzc9asQ; YSC=Xu6J_R5FWoM; VISITOR_INFO1_LIVE=U05uyeNkYqs"
 
 app = FastAPI()
+app2 = Flask(__name__)
 
-@app.get("/")
+#@app2.get("/")
+#@app2.route('/api', methods=['GET'])
+@app2.route('/api', methods=['GET', 'POST'])
 def read_root():
-    return {"message": cookies_path, "ServerStatus": "Server is running"}
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    user_agent = request.headers.get('User-Agent')
+    referer = request.headers.get('Referer')
+    accept = request.headers.get('Accept')
+    content_type = request.headers.get('Content-Type')
+    auth_token = request.headers.get('Authorization')
+    method = request.method
+    cookies = request.cookies
+
+    return {
+        "ip": ip,
+        "user_agent": user_agent,
+        "referer": referer,
+        "accept": accept,
+        "content_type": content_type,
+        "auth_token": auth_token,
+        "method": method,
+        "cookies": cookies
+    }, 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.get("/extract")
 def extract_url(video_url: str = Query(..., description="YouTube video URL")):
